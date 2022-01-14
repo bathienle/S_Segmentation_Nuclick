b9dca85b7dc777cb09eebbb4e558db196324dee3
Ba Thien Le <bathien.le.pro@gmail.com>
2022-01-14 17:36:20 +0100

    Update license date

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# * Copyright (c) 2009-2021. Authors: see NOTICE file.
+# * Copyright (c) 2021-2022. Authors: Ba Thien LE.
 # *
 # * Licensed under the Apache License, Version 2.0 (the "License");
 # * you may not use this file except in compliance with the License.
